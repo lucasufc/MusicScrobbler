d13b219158ee0093c4cf78fc44ecd55b678308f2
lucas <lucashk.eng@gmail.com>
2024-10-02 13:09:13 -0300

    feat: add react router dom

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {}
+import { Navigate, Route, Routes } from "react-router-dom"
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/home" element={"teste2"} />
+            <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+    )
+}
